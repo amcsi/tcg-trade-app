c539f03eed077a47a61497abc84222bfc17f8c70
Attila Szeremi <attila@szeremi.org>
2024-02-20 14:36:07 +0100

    Adjust light mode placeholder text color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,7 +4,7 @@ const tintColorDark = '#fff';
 export default {
   light: {
     text: '#000',
-    placeholderTextColor: undefined,
+    placeholderTextColor: 'rgb(210, 210, 210)',
     background: '#fff',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
